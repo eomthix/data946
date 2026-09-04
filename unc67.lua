c1058f5ba68be3cd6f796d008fdf7a94d962fd13
eomthix <Fortnite2684926@outlook.de>
2026-09-03 19:49:20 -0700

    Update unc67.lua

--- unc67.lua
+++ unc67.lua
@@ -166,7 +166,7 @@ return {
             ["mastery_emu"] = 0x20611C8, --1.73.6
             ["parts_phone"] = 0x2008758, --1.73.6
             ["parts_emu"] = 0x2063008, --1.73.6
-            ["tunes_lib_arm8"] = 0x2005688, --1.73.6
+            ["tunes_lib_arm8"] = 0x204AE38, --1.74.0
             ["tunes_lib_x64"] = 0x205FF38, --1.73.6
             ["unl_cups_phone"] = 0x204E698, --1.74.0
             ["unl_cups_emu"] = 0x2063638, --1.73.6
