37cca915cd5b9f1ff3e5681f26d7de26e94ee03b
eomthix <Fortnite2684926@outlook.de>
2026-09-26 12:54:36 +0200

    Update unc67.lua

--- unc67.lua
+++ unc67.lua
@@ -195,7 +195,7 @@ return {
 				["die_phone"] = 0x1A8F138,
 				["die_emu"] = 0x6CF834,
 				["countdown_phone"] = 0x21FB630,
-				["countdown_emu"] = 
+				["countdown_emu"] = 0x2256A40
             },
 
             ["PATCHES"] = {
